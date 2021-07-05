91b1f65f386b08ec9c5eb5b199b1d1ce71ddc6ba
eomjinyoung <jinyoung.eom@gmail.com>
2021-07-05 18:10:12 +0900

    .

---
+++ study-project/app-02-a/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,14 @@
+package com.eomcs.pms;
+
+public class App3 {
+
+  public static void main(String[] args) {
+    System.out.printf("[%s]\n", "작업");
+    System.out.printf("프로젝트: %s\n", "미니 프로젝트 관리 시스템 개발");
+    System.out.printf("번호: %d\n", 1);
+    System.out.printf("내용: %s\n", "요구사항 수집");
+    System.out.printf("완료일: %s\n", "2020-01-20");
+    System.out.printf("상태: %s\n", "진행중");
+    System.out.printf("담당자: %s\n", "홍길동");
+  }
+}
---
+++ study-project/app/src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,32 @@
+package com.eomcs.pms;
+
+public class App2 {
+
+  public static void main(String[] args) {
+    System.out.print("[");
+    System.out.print("프로젝트");
+    System.out.println("]");
+
+    System.out.print("번호: ");
+    System.out.println(1201);
+
+    System.out.print("프로젝트명: ");
+    System.out.println("미니 프로젝트 관리 시스템 개발");
+
+    System.out.print("내용: ");
+    System.out.println("소규모 팀을 위한 프로젝트 관리 시스템을 개발한다.");
+
+    System.out.println("시작일: " + "2020-01-01");
+
+    System.out.println("종료일: " + "2020-12-31");
+
+    System.out.println("만든이: " + "홍길동");
+    
+    System.out.println("팀원: "
+        + "홍길동" + ","
+        + "김구" + ","
+        + "유관순" + ","
+        + "안중근" + ","
+        + "윤봉길");
+  }
+}
