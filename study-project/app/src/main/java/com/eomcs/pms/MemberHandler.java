f9196aef95c26e1a4880deee8859efaff6021d82
eomjinyoung <jinyoung.eom@gmail.com>
2021-07-16 15:44:07 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/MemberHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/MemberHandler.java
@@ -4,35 +4,40 @@ import java.sql.Date;
 
 public class MemberHandler {
 
-  static final int LENGTH = 100;
-  static int[] no = new int[LENGTH];
-  static String[] name = new String[LENGTH];
-  static String[] email = new String[LENGTH];
-  static String[] password = new String[LENGTH];
-  static String[] photo = new String[LENGTH];
-  static String[] tel = new String[LENGTH];
-  static Date[] registeredDate = new Date[LENGTH];
+  static final int MAX_LENGTH = 5;
+
+  // Member 인스턴스의 주소를 저장할 레퍼런스를 3개 생성한다.
+  static Member[] members = new Member[MAX_LENGTH];
   static int size = 0;
 
   static void add() {
     System.out.println("[회원 등록]");
 
-    no[size] = Prompt.inputInt("번호? ");
-    name[size] = Prompt.inputString("이름? ");
-    email[size] = Prompt.inputString("이메일? ");
-    password[size] = Prompt.inputString("암호? ");
-    photo[size] = Prompt.inputString("사진? ");
-    tel[size] = Prompt.inputString("전화? ");
-    registeredDate[size] = new Date(System.currentTimeMillis());
+    // 새 회원 정보를 담을 변수를 준비한다.
+    // 낱 개의 변수가 아니라 Member에 정의된 대로 묶음 변수를 만든다.
+    Member member = new Member();
 
-    size++;
+    member.no = Prompt.inputInt("번호? ");
+    member.name = Prompt.inputString("이름? ");
+    member.email = Prompt.inputString("이메일? ");
+    member.password = Prompt.inputString("암호? ");
+    member.photo = Prompt.inputString("사진? ");
+    member.tel = Prompt.inputString("전화? ");
+    member.registeredDate = new Date(System.currentTimeMillis());
+
+    members[size++] = member;
   }
 
   static void list() {
     System.out.println("[회원 목록]");
     for (int i = 0; i < size; i++) {
       System.out.printf("%d, %s, %s, %s, %s\n", 
-          no[i], name[i], email[i], tel[i], registeredDate[i]);
+          members[i].no, 
+          members[i].name, 
+          members[i].email, 
+          members[i].tel, 
+          members[i].registeredDate);
     }
+
   }
 }
