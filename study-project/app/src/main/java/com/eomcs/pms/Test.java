c52f963f0881c0e224215908a753a0e9bd075c95
eomjinyoung <jinyoung.eom@gmail.com>
2021-07-22 16:42:15 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/Test.java
+++ study-project/app/src/main/java/com/eomcs/pms/Test.java
@@ -2,6 +2,9 @@ package com.eomcs.pms;
 
 public class Test {
 
+  // 배열에서 해당 항목의 값을 삭제한다.
+  // 배열 뒤의 항목은 -1로 설정한다.
+  // 배열을 출력할 때는 -1인 항목은 출력하지 않는다.
   static void deleteValue(int[] arr, int value) {
 
   }
@@ -11,6 +14,8 @@ public class Test {
     deleteValue(arr, 200);
 
     for (int value : arr) {
+      if (value == -1)
+        break;
       System.out.println(value);
     }
   }
