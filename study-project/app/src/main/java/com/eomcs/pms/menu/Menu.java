6365ab8a40ef29412b49c7489e2dc4fca6639958
eomjinyoung <jinyoung.eom@gmail.com>
2021-08-02 17:59:58 +0900

    08-b

---
+++ study-project/app/src/main/java/com/eomcs/pms/menu/Menu.java
@@ -0,0 +1,22 @@
+package com.eomcs.pms.menu;
+
+// MenuItem과 MenuGroup을 같은 타입으로 묶기 위해 정의한 클래스이다.
+// 이 클래스 자체는 직접 인스턴스를 만들어 사용하기 위함이 아니라
+// 상속해주는 용도이기 때문에 추상 클래스로 정의한다.
+public abstract class Menu {
+
+  String title;
+
+  // 메뉴 이름없이 인스턴스를 생성할 수 없도록 
+  // 기본 생성자를 정의하지 않는다.
+  // 대신 인스턴스를 만들 때 반드시 메뉴 이름을 입력하도록 강요하기 위해
+  // 다음과 같이 String 을 파라미터로 받는 생성자를 정의한다.
+  public Menu(String title) {
+    this.title = title;
+  }
+
+  // 서브 클래스에서 해야할 일을 정의한다.
+  // 단, 서브 클래스에서 반드시 재정의 할 메서드이기 때문에 
+  // 구체적으로 구현하지 않는다.
+  public abstract void execute();
+}
---
+++ study-project/app/src/main/java/com/eomcs/pms/menu/MenuGroup.java
@@ -0,0 +1,54 @@
+package com.eomcs.pms.menu;
+
+public class MenuGroup extends Menu {
+  Menu[] childs = new Menu[100];
+  int size;
+
+  // 생성자를 정의하지 않으면 컴파일러가 기본 생성자를 자동으로 추가해 준다.
+  // 문제는 컴파일러가 추가한 기본 생성자는 수퍼 클래스의 기본 생성자를 호출하기 때문에
+  // 컴파일 오류가 발생한다. 
+  // Menu 클래스에는 기본 생성자가 없다. 
+  // 따라서 개발자가 직접 생성자를 정의해야 한다.
+  public MenuGroup(String title) {
+    super(title);
+  }
+
+  // MenuGroup이 포함하는 하위 Menu를 다룰 수 있도록 메서드를 정의한다.
+  public void add(Menu childMenu) {
+    if (this.size == this.childs.length) {
+      return; // 하위 메뉴를 저장할 배열이 꽉 찼다면 더이상 저장해서는 안된다.
+    }
+    this.childs[this.size++] = childMenu; 
+  }
+
+  // 배열에 들어 있는 Menu 객체를 찾아 제거한다.
+  public void remove(Menu child) {
+    int index = indexOf(child);
+    for (int i = index + 1; i < this.size; i++) {
+      this.childs[i - 1] = this.childs[i];
+    }
+    childs[--this.size] = null;
+  }
+
+  // 배열에 들어 있는 Menu 객체의 인덱스를 알아낸다.
+  public int indexOf(Menu child) {
+    for (int i = 0; i < this.size; i++) {
+      if (this.childs[i] == child) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // 배열에 들어 있는 Menu 객체를 찾는다.
+  public Menu getMenu(String title) { 
+    // 완성하시오!
+    return null;
+  }
+
+  @Override // 컴파일러에게 오버라이딩을 제대로 하는지 조사해 달라고 요구한다.
+  public void execute() {
+
+  }
+
+}
