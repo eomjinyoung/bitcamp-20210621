fef408823dc72f4ff48666d3a6546282a287d085
eomjinyoung <jinyoung.eom@gmail.com>
2021-08-18 18:03:52 +0900

    .

--- java-basic/src/main/java/StudentSelector.java
+++ java-basic/src/main/java/StudentSelector.java
@@ -2,8 +2,8 @@ public class StudentSelector {
   public static void main(String[] args) throws Exception {
     String[] names = {"배서연", "김주창", "박성준", "구백연",
         "이서은", "방우주", "전예린", "이지호", "조대희", "최보균",
-        "반계령", "변민성", "김제이", "신현지", /*"윤수민",*/ /*"홍지현",*/
-        "조주리", "김진현", "김태호", "조솔", "위정욱", /*"이용진",*/
+        "반계령", "변민성", "김제이", "신현지", "윤수민", "홍지현",
+        "조주리", "김진현", "김태호", "조솔", "위정욱", "이용진",
         "송다혜", "하선영", "우형민", "이혜연", "김은채", "문세철"};
 
     for (int i = 0; i < 1000; i++) {
@@ -15,7 +15,7 @@ public class StudentSelector {
     }
 
     int selectedNo = -1;
-    for (int i = 10; i > 0; i--) {
+    for (int i = 2; i > 0; i--) {
       selectedNo = (int)(Math.random() * names.length);
       System.out.println(i);
       Thread.sleep(1000);
