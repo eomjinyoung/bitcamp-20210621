59dbf49f32c16683dcdcab267fab5982829d6bed
eomjinyoung <jinyoung.eom@gmail.com>
2021-09-07 12:24:45 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -116,7 +116,48 @@ public class App {
     // 게시글 데이터를 파일로 내보내기(저장하기, 쓰기)
     try (FileOutputStream out = new FileOutputStream("board.data")) {
       for (Board board : boardList) {
+        // 1) 게시글 번호 4바이트 출력
+        out.write(board.getNo() >> 24);
+        out.write(board.getNo() >> 16);
+        out.write(board.getNo() >> 8);
         out.write(board.getNo());
+
+        // 2) 게시글 제목 출력
+        byte[] bytes = board.getTitle().getBytes("UTF-8");
+        out.write(bytes.length >> 8); // 바이트의 개수를 2바이트로 먼저 출력한다.
+        out.write(bytes.length);
+        out.write(bytes); // 그런 후에 바이트를 출력한다.
+
+        // 3) 게시글 내용 출력
+        bytes = board.getContent().getBytes("UTF-8");
+        out.write(bytes.length >> 8); // 2바이트로 바이트 배열의 크기를 먼저 출력한다.
+        out.write(bytes.length); 
+        out.write(bytes); // 그런 후에 바이트 배열을 출력한다.
+
+        // 4) 게시글 등록일 출력
+        String dateStr = board.getRegisteredDate().toString(); // "2021-09-07"
+        bytes = dateStr.getBytes("UTF-8"); 
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        // 5) 게시글 조회수 출력
+        out.write(board.getViewCount() >> 24);
+        out.write(board.getViewCount() >> 16);
+        out.write(board.getViewCount() >> 8);
+        out.write(board.getViewCount());
+
+        // 6) 게시글 작성자 번호 출력
+        out.write(board.getWriter().getNo() >> 24);
+        out.write(board.getWriter().getNo() >> 16);
+        out.write(board.getWriter().getNo() >> 8);
+        out.write(board.getWriter().getNo());
+
+        // 7) 게시글 작성자 이름 출력
+        bytes = board.getWriter().getName().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
       }
     } catch (Exception e) {
       System.out.println("게시글을 파일에 저장 중 오류 발생!");
