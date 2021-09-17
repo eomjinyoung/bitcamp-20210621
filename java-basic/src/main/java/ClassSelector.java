4a84371f6255425753db2b6f62367070f97630ab
eomjinyoung <jinyoung.eom@gmail.com>
2021-09-17 14:34:15 +0900

    .

--- java-basic/src/main/java/ClassSelector.java
+++ java-basic/src/main/java/ClassSelector.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
 public class ClassSelector {
   public static void main(String[] args) throws Exception {
-    String[] names = {"1팀", "2팀", "3팀", "4팀", "5팀", "6팀"};
+    String[] names = {
+        "변민성,위정욱,박성준,구백연,이지호", 
+        "김은채,송다혜,조솔,우형민", 
+        "김태호,조주리,신현지,김진현", 
+        "윤수민,이용진,홍지현,문세철", 
+        "최보균,이혜연,방우주,이서은,조대희", 
+        "반계령,하선영,김제이,김주창"
+    };
 
     for (int i = 0; i < 1000; i++) {
       int a = (int)(Math.random() * names.length);
@@ -10,14 +19,26 @@ public class ClassSelector {
       names[b] = temp;
     }
 
-    int selectedNo = -1;
-    for (int i = 10; i > 0; i--) {
-      selectedNo = (int)(Math.random() * names.length);
-      System.out.println(i);
-      Thread.sleep(1000);
+    ArrayList<String> list = new ArrayList<>();
+    for (String name : names) {
+      list.add(name);
     }
 
-    System.out.printf("당첨: %s\n", names[selectedNo]);
+    while (list.size() > 0) {
+      Thread.sleep(2000);
+      int selectedNo = (int)(Math.random() * list.size());
+      System.out.println(list.remove(selectedNo));
+    }
+
+    //    int selectedNo = -1;
+    //    for (int i = 10; i > 0; i--) {
+    //      selectedNo = (int)(Math.random() * names.length);
+    //      System.out.println(i);
+    //      Thread.sleep(1000);
+    //    }
+    //
+    //    System.out.printf("당첨: %s\n", names[selectedNo]);
+
 
 
   }
--- java-basic/src/main/java/StudentSelector2.java
+++ java-basic/src/main/java/StudentSelector2.java
@@ -3,35 +3,36 @@ import java.util.Scanner;
 
 public class StudentSelector2 {
   public static void main(String[] args) throws Exception {
-    //    String[] names = {"배서연", "김주창", "박성준", "구백연",
-    //        "이서은", "방우주", "전예린", "이지호", "조대희", "최보균",
-    //        "반계령", "변민성", "김제이", "신현지", "윤수민", "홍지현",
-    //        "조주리", "김진현", "김태호", "조솔", "위정욱", "이용진",
-    //        "송다혜", "하선영", "우형민", "이혜연", "김은채", "문세철"};
-
-    //    String[] names = {"배서연", "김주창", 
-    //        "전예린", 
-    //        "반계령", "김제이", "신현지", 
-    //        "조주리", "김진현", "김태호", "조솔",
-    //        "송다혜", "하선영", "우형민", "김은채"};
-
-    String[] names = {"박성준", "구백연",
+    String[] names = {"김주창", "박성준", "구백연",
         "이서은", "방우주", "이지호", "조대희", "최보균",
-        "변민성", "윤수민", "홍지현",
-        "위정욱", "이용진",
-        "이혜연", "문세철"};
+        "반계령", "변민성", "김제이", "신현지", "윤수민", "홍지현",
+        "조주리", "김진현", "김태호", "조솔", "위정욱", "이용진",
+        "송다혜", "하선영", "우형민", "이혜연", "김은채", "문세철"};
 
-    ArrayList<String> students = new ArrayList<>();
-    for (String name : names) {
-      students.add(name);
-    }
+    //    String[] names = {"김태호","조주리","신현지","김진현",
+    //        "최보균","이혜연","방우주","이서은","조대희",
+    //        "김은채","송다혜","조솔","우형민"};
+    //    String[] names = {
+    //        "반계령","하선영","김제이","김주창",
+    //        "변민성","위정욱","박성준","구백연","이지호",
+    //        "윤수민","이용진","홍지현","문세철"
+    //    };
 
     for (int i = 0; i < 1000; i++) {
       int a = (int)(Math.random() * names.length);
       int b = (int)(Math.random() * names.length);
-      String temp = students.get(a);
-      students.set(a, students.get(b));
-      students.set(b, temp);
+      String temp = names[a];
+      names[a] = names[b];
+      names[b] = temp;
+    }
+
+    for (String name : names) {
+      System.out.print(name + " ");
+    }
+
+    ArrayList<String> students = new ArrayList<>();
+    for (String name : names) {
+      students.add(name);
     }
 
     Scanner keyboardScan = new Scanner(System.in);
@@ -45,10 +46,6 @@ public class StudentSelector2 {
 
     while (students.size() > 0) {
       int selectedNo = (int)(Math.random() * students.size());
-      //      for (int i = 0; i < 5; i++) {
-      //        System.out.print(".");
-      //        Thread.sleep(1000);
-      //      }
       System.out.println(students.remove(selectedNo));
       keyboardScan.nextLine();
     }
