b0e386312c991b555a977fe0556edf492bbe5a9d
eomjinyoung <jinyoung.eom@gmail.com>
2021-09-30 11:11:46 +0900

    .

--- study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -34,6 +34,7 @@ import com.eomcs.pms.handler.ProjectListHandler;
 import com.eomcs.pms.handler.ProjectPrompt;
 import com.eomcs.pms.handler.ProjectUpdateHandler;
 import com.eomcs.pms.handler.TaskAddHandler;
+import com.eomcs.pms.handler.TaskDetailHandler;
 import com.eomcs.pms.handler.TaskListHandler;
 import com.eomcs.pms.listener.AppInitListener;
 import com.eomcs.request.RequestAgent;
@@ -132,6 +133,7 @@ public class ClientApp {
     ProjectPrompt projectPrompt = new ProjectPrompt(requestAgent);
     commandMap.put("/task/add", new TaskAddHandler(requestAgent, projectPrompt));
     commandMap.put("/task/list", new TaskListHandler(projectPrompt));
+    commandMap.put("/task/detail", new TaskDetailHandler(projectPrompt));
   }
 
   // MenuGroup에서 사용할 필터를 정의한다.
